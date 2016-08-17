ebaa87121ba7368eb6a61a338a839b1b91d14e80
T.C <thomas.n.cool@gmail.com>
2016-08-17 10:07:06 -0700

    Create 2d-array.py

---
+++ 2d-array.py
@@ -0,0 +1,23 @@
+import sys
+
+
+arr = []
+for arr_i in xrange(6):
+    arr_temp = map(int,raw_input().strip().split(' '))
+    arr.append(arr_temp)
+
+hour = []
+a = x = 0
+b = y = 1
+
+while x < 4:
+    while a < 4:
+        hour.append(sum([arr[x][j] for j in range(a, a+3)]) + arr[y][b] + sum([arr[x+2][j] for j in range(a, a+3)]))
+        a += 1
+        b += 1
+    x += 1
+    y += 1
+    a = 0
+    b = 1
+
+print max(hour)
